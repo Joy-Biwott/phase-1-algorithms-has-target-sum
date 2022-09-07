6862e31bf98b7040434afe98a53215510de14bdb
Joy <biwott.joy.03@gmail.com>
2022-09-07 07:23:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  //iterate through the array
+  for (let startIndex = 0; startIndex < array.length; startIndex++){
+    const complement = target - array[startIndex];
+    for (let endIndex = startIndex + 1; endIndex<array.length; endIndex++){
+      if (array[endIndex] === complement){
+        return true;
+      }
+    }
+    }
+    return false
 }
 
 /* 
   Write the Big O time complexity of your function here
 */
 
+
 /* 
   Add your pseudocode here
+  iterate through the array
+  for the current number identify a complement
+  check if number is our complement
 */
+
 
 /*
   Add written explanation of your solution here
+  if the numer is our complement, it returns true, else false
 */
 
 // You can run `node index.js` to view these console logs
